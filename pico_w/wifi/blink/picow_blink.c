12a5168c77e8d464885f987f015a841ea080c05f
fildsady <anonx.filder@gmail.com>
2024-08-07 00:48:34 +0700

    pico W

--- pico_w/wifi/blink/picow_blink.c
+++ pico_w/wifi/blink/picow_blink.c
@@ -17,6 +17,6 @@ int main() {
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
         sleep_ms(500);
         cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(500);
+        sleep_ms(5020);
     }
 }
